4641e2ba96b8082e23ee235ee14950b97cca5979
RasmusLumholdt <dev.rlumholdt@gmail.com>
2019-06-10 15:19:12 +0200

    Added a find user example

--- src/GraphHandler.js
+++ src/GraphHandler.js
@@ -1,4 +1,4 @@
-import { Query, Mutation } from "react-apollo";
+import { Query, Mutation, ApolloConsumer } from "react-apollo";
 import { gql } from "apollo-boost";
 import React from 'react';
 
@@ -11,28 +11,30 @@ class GraphHandler extends React.Component {
     }
 
     onButtonClick = (e) => {
-    e.preventDefault();
-    //get target id to figure out what to render
-    this.setState({ whatToRender: e.target.id})
+        e.preventDefault();
+        //get target id to figure out what to render
+        this.setState({ whatToRender: e.target.id })
     }
 
     render() {
-        return(
+        return (
             <div>
                 <button id="allUsers" onClick={this.onButtonClick}>Click here to show users</button>
                 <button id="createUser" onClick={this.onButtonClick}>create User</button>
+                <button id="findUser" onClick={this.onButtonClick}>Find User</button>
                 {this.state.whatToRender === 'allUsers' && <GetAllUsers />}
                 {this.state.whatToRender === 'createUser' && <CreateUser />}
-                
+                {this.state.whatToRender === 'findUser' && <DelayedQuery userName="admin2"/>}
+
             </div>
         )
     }
 };
 
 const GetAllUsers = () => {
-    return (   
-    <Query
-        query={
+    return (
+        <Query
+            query={
                 gql`{
             users{
                 firstName
@@ -40,22 +42,24 @@ const GetAllUsers = () => {
             }
          }
         `
-        }
-    >
-    {({loading, error, data}) => {
-        if (loading) return <p>LOADER...</p>;
-        if (error) return <p>{error}</p>;
+            }
+        >
+            {({ loading, error, data }) => {
+                if (loading) return <p>LOADER...</p>;
+                if (error) return <p>{error}</p>;
 
-        return data.users.map(({firstName, _id}) => (
-        <div key={_id}>
-            <p>{firstName}</p>
-        </div>));
-    }
-    }
-    </Query>
+                return data.users.map(({ firstName, _id }) => (
+                    <div key={_id}>
+                        <p>{firstName}</p>
+                    </div>));
+            }
+            }
+        </Query>
     );
 
 }
+
+
 
 class CreateUser extends React.Component {
     constructor(props) {
@@ -70,8 +74,8 @@ class CreateUser extends React.Component {
     }
 
     render() {
-        const {firstName, lastName, userName, email, password} = this.state;
-        return(
+        const { firstName, lastName, userName, email, password } = this.state;
+        return (
             <Mutation mutation={
                 gql`
                     mutation AddNewUser($firstName: String!, $lastName: String!, $userName: String!, $email: String!, $password: String!) {
@@ -82,57 +86,99 @@ class CreateUser extends React.Component {
                 
                 `
             }>
-            {(AddNewUser, {data}) => (
-                <div>
-                    
-                    <form onSubmit={e => {
-                        console.log({firstName, lastName, userName, email, password});
-                        e.preventDefault();
-                        AddNewUser({variables: {firstName: firstName, lastName: lastName, userName: userName, email: email, password: password}});
-                    }}>
-                        <input
-                        value={firstName}
-                        onChange={e => this.setState({ firstName: e.target.value })}
-                        type="text"
-                        placeholder="First Name"
-                        />
-                        <input
-                        value={lastName}
-                        onChange={e => this.setState({ lastName: e.target.value })}
-                        type="text"
-                        placeholder="Last Name"
-                        />
-                        <input
-                        value={userName}
-                        onChange={e => this.setState({ userName: e.target.value })}
-                        type="text"
-                        placeholder="Username"
-                        />
-                        <input
-                        value={email}
-                        onChange={e => this.setState({ email: e.target.value })}
-                        type="text"
-                        placeholder="Email"
-                        />
-                        <input
-                        value={password}
-                        onChange={e => this.setState({ password: e.target.value })}
-                        type="text"
-                        placeholder="Password"
-                        />
-                        <button type="submit">CreateUser</button>
-                    </form>
-                    
-                    {data && <p>{data.addNewUser.firstName} created</p>}
-                    
-                </div>
-                
-            )}
+                {(AddNewUser, { data }) => (
+                    <div>
+
+                        <form onSubmit={e => {
+                            console.log({ firstName, lastName, userName, email, password });
+                            e.preventDefault();
+                            AddNewUser({ variables: { firstName: firstName, lastName: lastName, userName: userName, email: email, password: password } });
+                        }}>
+                            <input
+                                value={firstName}
+                                onChange={e => this.setState({ firstName: e.target.value })}
+                                type="text"
+                                placeholder="First Name"
+                            />
+                            <input
+                                value={lastName}
+                                onChange={e => this.setState({ lastName: e.target.value })}
+                                type="text"
+                                placeholder="Last Name"
+                            />
+                            <input
+                                value={userName}
+                                onChange={e => this.setState({ userName: e.target.value })}
+                                type="text"
+                                placeholder="Username"
+                            />
+                            <input
+                                value={email}
+                                onChange={e => this.setState({ email: e.target.value })}
+                                type="text"
+                                placeholder="Email"
+                            />
+                            <input
+                                value={password}
+                                onChange={e => this.setState({ password: e.target.value })}
+                                type="text"
+                                placeholder="Password"
+                            />
+                            <button type="submit">CreateUser</button>
+                        </form>
+
+                        {data && <p>{data.addNewUser.firstName} created</p>}
+
+                    </div>
+
+                )}
             </Mutation>
         )
     }
 }
 
+class DelayedQuery extends React.Component {
+    state = { user: null, username: '' };
 
+    onUserFetched = user => this.setState(() => ({ user }));
+
+    render() {
+        return (
+            <ApolloConsumer>
+                {client => (
+                    <div>
+                        
+                        <input
+                            onChange={e => this.setState({ username: e.target.value })}
+                            type="text"
+                            placeholder="Username"
+                        />
+                        <button
+                            //On click define the query to run
+                            onClick={async () => {
+                                const { data } = await client.query({
+                                    query: FIND_USER,
+                                    variables: { userName: this.state.username }
+                                });
+                                //Set user state to user recived from gql
+                                this.onUserFetched(data.user);
+                            }}
+                        >
+                            Search</button>
+                        {this.state.user && <p>{this.state.user.firstName}</p> /*Render User when it is in state*/}
+                    </div>
+                )}
+            </ApolloConsumer>
+        );
+    }
+}
+
+const FIND_USER = gql`
+            query User($userName: String!){
+                user(userName: $userName){
+                    firstName
+                    userName
+                }
+            }`
 
 export default GraphHandler;
